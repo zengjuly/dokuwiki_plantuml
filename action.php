f06207bef8758b40bb13bd3f2c5e606488a631d1
zengjuly <zengjuly@aliyun.com>
2024-03-10 14:42:59 +0800

    Update action.php

--- action.php
+++ action.php
@@ -19,7 +19,7 @@ class action_plugin_plantuml extends DokuWiki_Action_Plugin {
     /**
      * Register the event handler
      */
-    function register(&$controller) {
+    function register(Doku_Event_Handler $controller)  {
       if($this->getConf('button_enabled') == '1')
         $controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'insert_button', array ());
     }
@@ -27,7 +27,7 @@ class action_plugin_plantuml extends DokuWiki_Action_Plugin {
     /**
      * Inserts the toolbar button
      */
-    function insert_button(& $event, $param) {
+    function insert_button(Doku_Event &$event, $param) {
  		$event->data[] = array (
             'type' => 'format',
             'title' => htmlspecialchars($this->getLang('tooltip')),
